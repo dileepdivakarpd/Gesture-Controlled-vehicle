7ac557cd4d710d3a15064a29dd9f2e92364220e0
dileepdivakarpd <dileepdivakarpd@gmail.com>
2017-04-09 19:01:31 +0530

    Update new_gest_reciever.ino

--- new_gest_reciever/new_gest_reciever.ino
+++ new_gest_reciever/new_gest_reciever.ino
@@ -1,4 +1,4 @@
-#define left_wheel_f 3               //left wheel forward pin
+ #define left_wheel_f 3               //left wheel forward pin
 #define right_wheel_f 5              //right wheel forward pin
 #define left_wheel_b 6               //left wheel backward pin
 #define right_wheel_b 9              //right wheel backward pin
@@ -9,7 +9,7 @@ boolean led=HIGH;
 boolean countl=0,countr=0,countv=0;                     
 int l_indic=0,r_indic=0;             //indication
 int l_speed1=0,r_speed1=0,l_speed=0,r_speed=0;             //received from the transmitter
-int l_speed_hist=0,r_speed_hist=0;   //speed history
+  int l_speed_hist=0,r_speed_hist=0;   //speed history
 int l_speed_curr=0,r_speed_curr=0;   //current speed
 int max_speed=100;                   //maximum speed
 int loop_count=0;                    //loop count
@@ -44,9 +44,9 @@ void loop()
     if(Serial.read()==255)
     {
       while(Serial.available()==0);
-      l_speed1=Serial.read();//Serial.print(l_speed);Serial.print('\t');
+      l_speed1=Serial.read();Serial.print(l_speed1);Serial.print('\t');
       while(Serial.available()==0);
-      r_speed1=Serial.read();//Serial.println(r_speed);
+      r_speed1=Serial.read();Serial.println(r_speed1);
       
     }
   }
@@ -211,11 +211,11 @@ void indicator_func()
         case 0 : indic_delay=millis();countr++;break;
         case 1 : 
         {      
-          if((millis()-indic_delay)>1000)
+          if((millis()-indic_delay)>500)
         {
                 led=~led;
                 digitalWrite(right_indic,led);
-                countr++;Serial.println(countr);
+                countr++;//Serial.println(countr);
         }
         }break;
         default : countr=0;
@@ -230,11 +230,11 @@ void indicator_func()
         case 0 : indic_delay=millis();countl=1;break;
         case 1 : 
         {      
-          if((millis()-indic_delay)>1000)
+          if((millis()-indic_delay)>500)
         {
               led=~led;
               digitalWrite(left_indic,led);
-                countl=0;Serial.println('l');
+                countl=0;//Serial.println('l');
         }
         }break;
         default : countl=0;
@@ -254,7 +254,7 @@ void indicator_func()
               led=~led;
         digitalWrite(left_indic,led);
         digitalWrite(right_indic,led);
-                countv=0;Serial.println('v');
+                countv=0;//Serial.println('v');
            
         }break;
         }
@@ -265,15 +265,15 @@ void indicator_func()
     case 'b' : 
     {
       digitalWrite(break_indic,HIGH);
-      break_delay=millis();Serial.println('b');
+      break_delay=millis();//Serial.println('b');
     }
     default :
     {
       if(((millis()-indic_delay)>4000)|(indic_delay=0))
       {
         //indic_delay=0;
-      digitalWrite(left_indic,LOW);Serial.print(countl);Serial.print(countr);Serial.print(countv);
-      digitalWrite(right_indic,LOW);Serial.println('d');
+      digitalWrite(left_indic,LOW);//Serial.print(countl);Serial.print(countr);Serial.print(countv);
+      digitalWrite(right_indic,LOW);//Serial.println('d');
       //digitalWrite(rev_indic,LOW);
       }
     }
